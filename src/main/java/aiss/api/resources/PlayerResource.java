515ec5e845eb232efc7decfa1aaf51622a58ee81
Unknown <themastah1234@gmail.com>
2017-05-16 18:31:55 +0200

    Métodos creados (falta GET filtrado)

--- src/main/java/aiss/api/resources/PlayerResource.java
+++ src/main/java/aiss/api/resources/PlayerResource.java
@@ -48,6 +48,36 @@ public class PlayerResource {
 			"Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vi", "Viktor", "Vladimir", "Volibear"
 			,"Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo", "Yorick", "Zac", "Zed", "Ziggs", 
 			"Zilean", "Zyra"));
+	public static final List<String> TIER_POOL = new ArrayList<String>
+	(Arrays.asList("Bronce", "Silver", "Gold", "Platinum", "Diamond", "Master", "Challenger"));
+	public static final List<String> DIVISION_POOL = new ArrayList<String>
+	(Arrays.asList("I", "II", "III", "IV", "V"));
+	public static final List<String> ROLE_POOL = new ArrayList<String>
+	(Arrays.asList("Top", "Jungler", "Mid", "Marksman", "Support"));
+	
+	private boolean areChampsOk(Player player) {
+		
+		Boolean res = true;
+		for (String champion : player.getChampions()){
+			if (!CHAMP_POOL.contains(champion)){
+				res = false;
+				break;
+			}
+		}
+		return res;
+	}
+	
+	private boolean isDivisionOk(Player player) {
+		
+		Boolean res = DIVISION_POOL.contains(player.getDivision());
+		return res;
+	}
+	
+	private boolean isTierOk(Player player) {
+		
+		Boolean res = TIER_POOL.contains(player.getTier());
+		return res;
+	}
 	
 	public static PlayerResource _instance=null;
 	PlayerRepository repository;
@@ -63,6 +93,7 @@ public class PlayerResource {
 		return _instance; 
 	}
 	
+	// Recibir todos los jugadores
 	@GET
 	@Produces("application/json")
 	public Collection<Player> getAll()
@@ -70,7 +101,7 @@ public class PlayerResource {
 		return repository.getAllPlayers();
 	}
 	
-	
+	// Recibir un jugador según ID
 	@GET
 	@Path("/{id}")
 	@Produces("application/json")
@@ -107,21 +138,89 @@ public class PlayerResource {
 				throw new BadRequestException("La división no debe ser nula o vacía");
 			}
 			
+			// Comprobación de que los valores introducidos son correctos para el juego.
 			
-		return null;
+			if(!areChampsOk(player)){
+				throw new BadRequestException("Los campeones no son válidos");
+			}
+			if(!isDivisionOk(player)){
+				throw new BadRequestException("La división no es válida");
+			}
+			if(!isTierOk(player)){
+				throw new BadRequestException("El tier no es correcto");
+			}
+			
+			if((player.getTier().equals("Challenger") || (player.getTier().equals("Master"))) 
+					&& !player.getDivision().equals("I")){
+				throw new BadRequestException("La división debe ser I para jugadores en Master y Challenger");
+			}
+			repository.addPlayer(player);
+			UriBuilder ub = uriInfo.getAbsolutePathBuilder().path(this.getClass(), "get");
+			URI uri = ub.build(player.getId());
+			ResponseBuilder resp = Response.created(uri);
+			resp.entity(player);
+			return resp.build();
 	}
 	
 	
+
+
 	@PUT
 	@Consumes("application/json")
 	public Response updateSong(Player player) {
-		return null;
-	}
+		
+		if(player.getSummonerName() == null || "".equals(player.getSummonerName())){
+			throw new BadRequestException("El nombre de invocador no debe ser nulo o vacío");
+		}
+		
+		if(player.getPassword() == null || "".equals(player.getPassword())){
+			throw new BadRequestException("La contraseña no debe ser nula o vacía");
+		}
+		
+		if(player.getTier() == null || "".equals(player.getTier())){
+			throw new BadRequestException("El tier no debe ser nulo o vacío");
+		}
+		
+		if(player.getLocation() == null || "".equals(player.getLocation())){
+			throw new BadRequestException("La localización no debe ser nula o vacía");
+		}
+		
+		if(player.getDivision() == null || "".equals(player.getDivision())){
+			throw new BadRequestException("La división no debe ser nula o vacía");
+		}
+		
+		// Comprobación de que los valores introducidos son correctos para el juego.
+		
+		if(!areChampsOk(player)){
+			throw new BadRequestException("Los campeones no son válidos");
+		}
+		if(!isDivisionOk(player)){
+			throw new BadRequestException("La división no es válida");
+		}
+		if(!isTierOk(player)){
+			throw new BadRequestException("El tier no es correcto");
+		}
+		
+		if((player.getTier().equals("Challenger") || (player.getTier().equals("Master"))) 
+				&& !player.getDivision().equals("I")){
+			throw new BadRequestException("La división debe ser I para jugadores en Master y Challenger");
+		}
+		
+		return Response.noContent().build();	}
 	
 	@DELETE
 	@Path("/{id}")
-	public Response removeSong(@PathParam("id") String player) {
-		return null;
+	public Response removeSong(@PathParam("id") String playerId) {
+		
+		Player playerRemoved = repository.getPlayer(playerId);
+		
+		if (playerRemoved == null) {
+			throw new NotFoundException("El jugador con id " + playerId +" no fue encontrado");
+		}else{
+			repository.deletePlayer(playerId);
+		}
+		
+		return Response.noContent().build();
 	}
 	
 }
